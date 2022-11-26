a573cad974d8bd72946a06308719d4c81fe3678b
Adnan Hussain <adnanhussain7995@gmail.com>
2022-11-27 02:45:28 +0500

    few changes

--- controllers/spreadsheet.js
+++ controllers/spreadsheet.js
@@ -1,70 +1,69 @@
 const Item = require('../model/model')
 const History = require('../model/history')
 var XLSX = require('xlsx');
+const { StatusCodes } = require('http-status-codes');
 // var path = require('path');
-const {Parser} = require('json2csv')
 
-const spreadsheet = async (req,res) =>{
-  
-    
-        //yahan jis feature ko search krogy us feature k products show hongy mere case mein feature (true or false)
-        //hain, true search krny pr sirf ture feature waly products result mein aengy
-        const { type  } = req.query
-       
-        const queryObject = {}
-        //yahan jis feature ko search krogy us feature k products show hongy mere case mein feature (true or false)
-        //hain, true search krny pr sirf ture feature waly products result mein aengy
-        // if (SerialNo){
-        //     queryObject.SerialNo = SerialNo
-        // }
-        // if (present_storenumber) {
-        //     queryObject.present_storenumber = present_storenumber
-        // }
-        // if (itemid) {
-        //     queryObject.itemid = itemid
-        // }
-        // if (rentee) {
-        //     queryObject.rentee = rentee
-        // }
-        //yahan jis company ka search krogy us company k products show honge
-        // if (status) {
-        //     queryObject.status = status
-        // }
-        // //yahan jis name se search krogy us name k sary products result mein aengy
-        // if (name) {
-        //     queryObject.name = { $regex: name, $options: 'i' }
-        // }
-      
+const spreadsheet = async (req, res) => {
+   const { type } = req.query;
 
-        
-        let result = type && type == 'logs' ? History.find({}) : Item.find({})
-      
+   //yahan jis feature ko search krogy us feature k products show hongy mere case mein feature (true or false)
+   //hain, true search krny pr sirf ture feature waly products result mein aengy
+
+   const queryObject = {}
+   //yahan jis feature ko search krogy us feature k products show hongy mere case mein feature (true or false)
+   //hain, true search krny pr sirf ture feature waly products result mein aengy
+   // if (SerialNo){
+   //     queryObject.SerialNo = SerialNo
+   // }
+   // if (present_storenumber) {
+   //     queryObject.present_storenumber = present_storenumber
+   // }
+   // if (itemid) {
+   //     queryObject.itemid = itemid
+   // }
+   // if (rentee) {
+   //     queryObject.rentee = rentee
+   // }
+   //yahan jis company ka search krogy us company k products show honge
+   // if (status) {
+   //     queryObject.status = status
+   // }
+   // //yahan jis name se search krogy us name k sary products result mein aengy
+   // if (name) {
+   //     queryObject.name = { $regex: name, $options: 'i' }
+   // }
 
 
-        // const page = Number(pageNo) || 1
-        // const limit = Number(count) || 10
-        // const skip = (page - 1) * limit
 
-        // result = result.skip(skip).limit(limit)
+   let result = type && type == 'logs' ? History.find({}) : Item.find({})
 
-        //console.log(queryObject)
-        const products = await result
-    var wb = XLSX.utils.book_new(); //new workbook
-    var temp = JSON.stringify(products);
-    temp = JSON.parse(temp);
-    var ws = XLSX.utils.json_to_sheet(temp);
-    var down = type && type == 'logs' ? './logsExport.xlsx' : './itemsExport.xlsx'
-           // C:/Users/Hussain Computer/Downloads/Documents
-            console.log(__dirname)
-           XLSX.utils.book_append_sheet(wb,ws,"sheet1");
-        //    XLSX.writeFile(wb,down);
-           res.download(down);
-        //    res.attachment(down)
-        //    res.status(200).send(XLSX.writeFile(wb,down))
-        //    res.send("File Downloaded")
-  }
-  
-  module.exports = {
-    spreadsheet
-  };
-  
+
+
+   // const page = Number(pageNo) || 1
+   // const limit = Number(count) || 10
+   // const skip = (page - 1) * limit
+
+   // result = result.skip(skip).limit(limit)
+
+   //console.log(queryObject)
+   const products = await result
+   var wb = XLSX.utils.book_new(); //new workbook
+   var temp = JSON.stringify(products);
+   temp = JSON.parse(temp);
+   var ws = XLSX.utils.json_to_sheet(temp);
+   //  var down = type && type == 'logs' ? './logsExport.xlsx' : './itemsExport.xlsx'
+   // C:/Users/Hussain Computer/Downloads/Documents
+   // console.log(__dirname)
+   //   XLSX.utils.book_append_sheet(wb,ws,"sheet1");
+   //    XLSX.writeFile(wb,down);
+   res.download(XLSX.utils.book_append_sheet(wb, ws));
+   res.status(StatusCodes.OK);
+   //    res.attachment(down)
+   //    res.status(200).send(XLSX.writeFile(wb,down))
+   //    res.send("File Downloaded")
+}
+
+module.exports = {
+   spreadsheet
+};
--- routes/spreadsheet.js
+++ routes/spreadsheet.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
-const {spreadsheet } = require('../controllers/spreadsheet');
+const { spreadsheet } = require('../controllers/spreadsheet');
 const { authenticateUser, authorizePermissions } = require('../middleware/authentication');
 
 
-router.get('/spreadsheet', authenticateUser, spreadsheet);
+router.route('/spreadsheet').get(authenticateUser, spreadsheet);
 
 
 
