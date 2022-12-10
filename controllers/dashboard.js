5ce11ed7b63d650e4d4e2ae6e4605a65777f7303
Adnan Hussain <adnanhussain7995@gmail.com>
2022-12-11 04:17:22 +0500

    merged umar branch

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -17,17 +17,11 @@ const dashboard = async (req, res) => {
     const query3 = { status: "available", present_storenumber: "1" };
     const countNotRentedStoreOne = await Item.find(query3).countDocuments().exec()
 
-    const queryMainStoreOne = { status: "maintenance", present_storenumber: "1" };
-    const countMainStoreOne = await Item.find(queryMainStoreOne).countDocuments().exec()
-
     const query4 = { status: "rented", present_storenumber: "1" };
     const countRentedStoreOne = await Item.find(query4).countDocuments().exec()
 
     const query5 = { status: "available", present_storenumber: "2" };
     const countNotRentedStoreTwo = await Item.find(query5).countDocuments().exec()
-
-    const queryMainStoreTwo = { status: "maintenance", present_storenumber: "2" };
-    const countMainStoreTwo = await Item.find(queryMainStoreTwo).countDocuments().exec()
 
     const query6 = { status: "rented", present_storenumber: "2" };
     const countRentedStoreTwo = await Item.find(query6).countDocuments().exec()
@@ -35,17 +29,11 @@ const dashboard = async (req, res) => {
     const query7 = { status: "available", present_storenumber: "3" };
     const countNotRentedStoreThree = await Item.find(query7).countDocuments().exec()
 
-    const queryMainStoreThree = { status: "maintenance", present_storenumber: "3" };
-    const countMainStoreThree = await Item.find(queryMainStoreThree).countDocuments().exec()
-
     const query8 = { status: "rented", present_storenumber: "3" };
     const countRentedStoreThree = await Item.find(query8).countDocuments().exec()
 
     const query9 = { status: "available", present_storenumber: "4" };
     const countNotRentedStoreFour = await Item.find(query9).countDocuments().exec()
-
-    const queryMainStoreFour = { status: "maintenance", present_storenumber: "4" };
-    const countMainStoreFour = await Item.find(queryMainStoreFour).countDocuments().exec()
 
     const query10 = { status: "rented", present_storenumber: "4" };
     const countRentedStoreFour = await Item.find(query10).countDocuments().exec()
@@ -66,7 +54,7 @@ const dashboard = async (req, res) => {
         countNotRentedStoreOne ,countRentedStoreOne ,countNotRentedStoreTwo, 
         countRentedStoreTwo, countNotRentedStoreThree ,countRentedStoreThree,
         countNotRentedStoreFour ,countRentedStoreFour,countMaintenanceStoreOne,
-        countMaintenanceStoreTwo, countMaintenanceStoreThree, countMaintenanceStoreFour });
+        countMaintenanceStoreTwo, countMaintenanceStoreThree, countMaintenanceStoreFour, countTotalMaintained });
 }
 
 module.exports = {
